bae98bc831e501d23e03ba4366c0a268f9235e61
SCOTT <141160454+Scott-xd@users.noreply.github.com>
2023-11-22 16:56:58 -0500

    cambiar

--- services/maestros/colegiado.js
+++ services/maestros/colegiado.js
@@ -32,7 +32,7 @@ export class ColegiadoService {
     try {
       const data = await Colegiado.findOne({
         where: {
-          codigo: codigo,
+          id: codigo,
         },
       });
       return data;
